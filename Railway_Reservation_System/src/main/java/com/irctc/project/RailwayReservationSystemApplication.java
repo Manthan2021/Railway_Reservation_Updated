66289ce107d204a85125aa971afbfd02754af086
Manthan2021 <95281934+Manthan2021@users.noreply.github.com>
2024-06-08 16:03:36 +0530

    Update RailwayReservationSystemApplication.java

--- Railway_Reservation_System/src/main/java/com/irctc/project/RailwayReservationSystemApplication.java
+++ Railway_Reservation_System/src/main/java/com/irctc/project/RailwayReservationSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@Configuration
+
 public class RailwayReservationSystemApplication {
 	
 	@Bean
